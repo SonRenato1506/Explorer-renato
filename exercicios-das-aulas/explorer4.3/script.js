45f05a335681b06aa12ea7f619164db7ea36335e
Renato Matos <108771359+SonRenato1506@users.noreply.github.com>
2023-01-10 20:20:32 -0300

    10/01/2023 20:20

--- exercicios-das-aulas/explorer4.3/script.js
+++ exercicios-das-aulas/explorer4.3/script.js
@@ -1,49 +1,46 @@
-/*
-  Faça um programa que tenha um menu e apresente a seguinte mensagem:
-  
-  Olá usuário! Digite o número da opção desejada
+/* 
+  Crie uma lista de pacientes
 
-    1. Cadastrar um item na lista
-    2. Mostrar itens cadastrados
-    3. Sair do programa
-  
-  --- 
-  O programa deverá capturar o número digitado pelo usuário e mostrar o seguintes cenários:
+  Cada paciente dentro da lista, deverá conter
+    nome
+    idade
+    peso
+    altura
 
-  Caso o usuário digite 1, ele poderá cadastrar um item em uma lista
-  Caso o usuário digite 2, ele poderá ver os itens cadastrados
-    Se não houver nenhum item cadastrado, mostrar a mensagem: 
-      "Não existem itens cadastrados"
-  Caso o usuário digite 3, a aplicação deverá ser encerrada.
+  Escreva uma lista contendo o nome dos pacientes
 */
-let option
-let items = []
 
-while(option != 3) {
-
-  option =  Number(prompt(`
-    Olá usuário! Digite o número da opção desejada
-    
-    1. Cadastrar um item na lista
-    2. Mostrar itens cadastrados
-    3. Sair do programa
-  `))
-  
-  if(option == 1) {
-    let item = prompt("Digite o nome do item")
-    items.push(item) 
-  }
-  
-  else if (option == 2) {
-  
-    if(items.length == 0) {
-      alert("Não existem itens cadastrados")
-    } else {
-      alert(items)
+const patients = [
+    {
+        nome: "Renato",
+        idade: 14,
+        peso: 45,
+        altura: 1.78,  
+    },
+    {
+        nome: "Arthur",
+        idade: 0,
+        peso: 10,
+        altura: 0.60,
+    },
+    {
+        nome: "Mônica",
+        idade: 34,
+        peso: 70,
+        altura: 1.70,
+    },
+    {
+      nome: "Marcio",
+      idade: 35,
+      peso: 80,
+      altura: 1.83,
     }
-  
-  } else {
-    alert("Tchau")
-  }
+]
 
+let patientsNames = []
+
+for(let patient of patients) {
+  patientsNames.push(patient.nome)
 }
+
+alert(patientsNames)
