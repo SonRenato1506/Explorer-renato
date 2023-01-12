9e375d06cc45011b0039e2377aca978ca2d0eff4
Renato Matos <108771359+SonRenato1506@users.noreply.github.com>
2023-01-12 20:06:39 -0300

    Update script.js

--- exercicios-extra/4.3/par-ou-impar/script.js
+++ exercicios-extra/4.3/par-ou-impar/script.js
@@ -1,9 +1,9 @@
 let operation = prompt(`
-    Digite 1 para somar os 2 numeros
-    Digite 2 para subtrair os 2 numeros
-    Digite 3 para multiplicar os os 2 numeros
-    Digite 4 para dividir os 2 numeros
-    Digite 5 para ver o resto da divisão dos 2 numeros
+    Digite 1 para somar os 2 números
+    Digite 2 para subtrair os 2 números
+    Digite 3 para multiplicar os os 2 números
+    Digite 4 para dividir os 2 números
+    Digite 5 para ver o resto da divisão dos 2 números
     Digite 6 para sair
     `)
     let numberOne
@@ -24,7 +24,7 @@ switch (operation) {
                 parImpar = "impar"
                 break
         }
-        alert(numberOne + " + " + numberTwo + " = " + soma + " , e seu numero é " + parImpar)
+        alert(numberOne + " + " + numberTwo + " = " + soma + " , e seu número é " + parImpar)
         break
     case "2":
         numberOne = Number(prompt("Digite o primeiro número"))
@@ -39,7 +39,7 @@ switch (operation) {
                 parImpar = "impar"
                 break
         }
-        alert(numberOne + " - " + numberTwo + " = " + sub + " , e seu numero é " + parImpar)
+        alert(numberOne + " - " + numberTwo + " = " + sub + " , e seu número é " + parImpar)
         break
     case "3":
         numberOne = Number(prompt("Digite o primeiro número"))
@@ -54,7 +54,7 @@ switch (operation) {
                 parImpar = "impar"
                 break
         }
-        alert(numberOne + " * " + numberTwo + " = " + mult + " , e seu numero é " + parImpar)
+        alert(numberOne + " * " + numberTwo + " = " + mult + " , e seu número é " + parImpar)
         break
     case "4":
         numberOne = Number(prompt("Digite o primeiro número"))
@@ -69,7 +69,7 @@ switch (operation) {
                 parImpar = "impar"
                 break
         }
-        alert(numberOne + " / " + numberTwo + " = " + div + " , e seu numero é " + parImpar)
+        alert(numberOne + " / " + numberTwo + " = " + div + " , e seu número é " + parImpar)
         break
     case "5":
         numberOne = Number(prompt("Digite o primeiro número"))
@@ -84,7 +84,7 @@ switch (operation) {
                 parImpar = "impar"
                 break
         }
-        alert(`O resto da divisão de ${numberOne} por ${numberTwo} é ${res}  , e seu numero é  + ${parImpar}`)
+        alert(`O resto da divisão de ${numberOne} por ${numberTwo} é ${res}  , e seu número é  + ${parImpar}`)
         break
     default:
         alert("Tchau")
