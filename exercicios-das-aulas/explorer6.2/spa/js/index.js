5ec78fef8fdcf6093f23bc2caf27356f424669a7
Renato Matos <108771359+SonRenato1506@users.noreply.github.com>
2023-03-30 09:05:28 -0300

    Update index.js

--- exercicios-das-aulas/explorer6.2/spa/js/index.js
+++ exercicios-das-aulas/explorer6.2/spa/js/index.js
@@ -1,6 +1,9 @@
 import Router from './router.js'
 const router = new Router()
-router.add()
+router.add('/', "./pages/home.html")
+router.add('/about', "./pages/about.html")
+router.add('/contact', "./pages/contact.html")
+router.add(404, "./pages/404.html")
 // const routes = {
 //     "/": "./pages/home.html",
 //     "/about": "./pages/about.html",
