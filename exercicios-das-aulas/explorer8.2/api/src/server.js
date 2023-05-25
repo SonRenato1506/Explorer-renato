e4a9dd345151a7dfe157aaec80392a9923ed07b3
Renato <renatimnerdin@gmail.com>
2023-05-24 21:22:41 -0300

    21:22

--- exercicios-das-aulas/explorer8.2/api/src/server.js
+++ exercicios-das-aulas/explorer8.2/api/src/server.js
@@ -1,18 +1,8 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json())
 
-app.get("/message/:id/:user", (request, response) => {
-    const {id, user} = request.params;
-
-    response.send(`Mensagem ID: ${id}. Para o Usuário: ${user}.`)
-})
-
-app.get("/users", (request, response) => {
-    const {page, limit} = request.query;
-
-    responde.send(`Página ${page}. Mostrar: ${limit}`)
-})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
