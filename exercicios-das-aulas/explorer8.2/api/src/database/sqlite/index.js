523a8e75ca6b680b7a665cc443976db1a20586d8
Renato <renatimnerdin@gmail.com>
2023-05-26 09:05:37 -0300

    Update index.js

--- exercicios-das-aulas/explorer8.2/api/src/database/sqlite/index.js
+++ exercicios-das-aulas/explorer8.2/api/src/database/sqlite/index.js
@@ -1,4 +1,9 @@
 const sqlite3 = require("sqlite3")
 const sqlite = require("sqlite")
+const path = requite("path")
 
-async
+async function sqiliteConnection() {
+    const database =  await sqlite.open({
+        filename: ""
+    })
+}
