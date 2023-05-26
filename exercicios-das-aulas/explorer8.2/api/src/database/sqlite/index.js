aeb390f315da7d217476609c966ef1d171b0671a
Renato <renatimnerdin@gmail.com>
2023-05-26 09:55:39 -0300

    09:55

--- exercicios-das-aulas/explorer8.2/api/src/database/sqlite/index.js
+++ exercicios-das-aulas/explorer8.2/api/src/database/sqlite/index.js
@@ -1,14 +1,14 @@
 const sqlite3 = require("sqlite3")
 const sqlite = require("sqlite")
-const path = requite("path")
+const path = require("path")
 
-async function sqiliteConnection() {
+async function sqliteConnection() {
     const database =  await sqlite.open({
         filename: path.resolve(__dirname, "..", "database.db"),
-        driver: sqlite3.database
+        driver: sqlite3.Database
     })
 
     return database
 }
 
-module.exports = sqiliteConnection
+module.exports = sqliteConnection
