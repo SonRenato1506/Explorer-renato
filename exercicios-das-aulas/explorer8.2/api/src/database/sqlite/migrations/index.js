54fa965fd6a6defb9aa4ce33d23ef22141039911
Renato <renatimnerdin@gmail.com>
2023-05-26 20:43:26 -0300

    20:43

--- exercicios-das-aulas/explorer8.2/api/src/database/sqlite/migrations/index.js
+++ exercicios-das-aulas/explorer8.2/api/src/database/sqlite/migrations/index.js
@@ -4,5 +4,11 @@ const createUsers = require('./createUses')
 async function migrationsRun() {
     const schemas = [
         createUsers
-    ]
+    ].join('');
+
+    sqliteConnection()
+    .then(db => db.exec(schemas))
+    .catch(error => console.error(error))
 }
+
+module.exports = migrationsRun;
