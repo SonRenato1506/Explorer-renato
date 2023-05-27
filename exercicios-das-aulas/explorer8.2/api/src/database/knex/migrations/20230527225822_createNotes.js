3dc26bbf448762245f7a44d2b7480d8e1d6ff460
Renato <renatimnerdin@gmail.com>
2023-05-27 20:30:18 -0300

    20:30

--- exercicios-das-aulas/explorer8.2/api/src/database/knex/migrations/20230527225822_createNotes.js
+++ exercicios-das-aulas/explorer8.2/api/src/database/knex/migrations/20230527225822_createNotes.js
@@ -1,7 +1,11 @@
+exports.up = knex => knex.schema.createTable("notes", table => {
+    table.increments("id")
+    table.text("title")
+    table.text("description")
+    table.integer("user_id").references("id").inTable("users")
 
-exports.up = function(knex) {
-  
-};
-exports.down = function(knex) {
-  
-};
+    table.timestamp("created_at").default(knex.fn.now())
+    table.timestamp("uptaded_at").default(knex.fn.now())
+});
+
+exports.down = knex => knex.schema.dropTable("notes")
