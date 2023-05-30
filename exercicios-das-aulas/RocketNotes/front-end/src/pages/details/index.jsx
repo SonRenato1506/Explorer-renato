6bb545e91c05d57924e201207328aee9cbedb800
Renato <renatimnerdin@gmail.com>
2023-05-30 20:35:48 -0300

    20:35

--- exercicios-das-aulas/RocketNotes/front-end/src/pages/details/index.jsx
+++ exercicios-das-aulas/RocketNotes/front-end/src/pages/details/index.jsx
@@ -1,9 +1,13 @@
 import { Container } from "./styles"
+import { Button } from "../../components/Button"
 export function Details() {
   return (
     <Container>
       <h1>Hello Word</h1>
       <span>Renato Matos</span>
+      <Button title="entrar"/>
+      <Button title="cadastrar" />
+      <Button  title="voltar" />
     </Container>
   )
 }
