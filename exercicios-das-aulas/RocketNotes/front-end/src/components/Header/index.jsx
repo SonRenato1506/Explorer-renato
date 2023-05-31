eff472a2ebcb91b8a4a5d8f32fb2d3aff590d23e
Renato <renatimnerdin@gmail.com>
2023-05-31 08:59:34 -0300

    08:59

--- exercicios-das-aulas/RocketNotes/front-end/src/components/Header/index.jsx
+++ exercicios-das-aulas/RocketNotes/front-end/src/components/Header/index.jsx
@@ -1,8 +1,14 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 export function Header() {
     return (
          <Container>
-            
+            <Profile>
+                <img src="https://github.com/SonRenato1506.png" alt="Foto do usuário" />
+                <div>
+                    <span>Bem vindo!</span>
+                    <strong>Renato Matos</strong>
+                </div>
+            </Profile>
          </Container>
     )
 }
--- exercicios-das-aulas/RocketNotes/front-end/src/pages/details/styles.js
+++ exercicios-das-aulas/RocketNotes/front-end/src/pages/details/styles.js
@@ -5,4 +5,8 @@ export const Container = styled.div`
     height: 100vh;
     
     display: grid;
+    grid-template-rows: 105px auto;
+    grid-template-areas: 
+    "header"
+    "content"
 `;
