324fb76acc5541612ac661a2fc557f4c5592de87
Renato <renatimnerdin@gmail.com>
2023-05-31 08:23:08 -0300

    08:23

--- exercicios-das-aulas/RocketNotes/front-end/src/components/Button/index.jsx
+++ exercicios-das-aulas/RocketNotes/front-end/src/components/Button/index.jsx
@@ -1,10 +1,11 @@
 import { Container } from "./styles";
 
-export function Button({title, loading = fast, ...rest}) {
+export function Button({title, loading = false, ...rest}) {
     return (
     <Container
      type="button"
      disabled={loading}
+     {...rest}
      >
         {loading ? 'Carregando...':title}
     </Container>
--- exercicios-das-aulas/RocketNotes/front-end/src/pages/details/index.jsx
+++ exercicios-das-aulas/RocketNotes/front-end/src/pages/details/index.jsx
@@ -3,10 +3,7 @@ import { Button } from "../../components/Button"
 export function Details() {
   return (
     <Container>
-      <h1>Hello Word</h1>
-      <span>Renato Matos</span>
-      <Button title="Entrar" loading/>
-      <Button title="Cadastrar" />
+      
       <Button  title="Voltar" />
     </Container>
   )
