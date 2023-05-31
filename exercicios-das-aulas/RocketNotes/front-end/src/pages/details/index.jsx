d917c01630c61898990a8fa31a4caa6ea45634a4
Renato <renatimnerdin@gmail.com>
2023-05-31 20:32:41 -0300

    20:32

--- exercicios-das-aulas/RocketNotes/front-end/src/pages/details/index.jsx
+++ exercicios-das-aulas/RocketNotes/front-end/src/pages/details/index.jsx
@@ -1,6 +1,7 @@
 import { Container } from "./styles"
 import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
+import { Section } from "../../components/Section"
 export function Details() {
   return (
     <Container>
