15ba42f2c5ffc6d46a4d9cc3ee3d67a28e7836f6
Renato <renatimnerdin@gmail.com>
2023-06-03 20:55:30 -0300

    Fim da página Home e cmc da SignIn

--- exercicios-das-aulas/RocketNotes/front-end/src/main.jsx
+++ exercicios-das-aulas/RocketNotes/front-end/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Home } from './pages/home/index.jsx'
+import { SignIn } from './pages/SignIn/index.jsx'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme.js'
 import GlobalStyles from './styles/global.js'
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ exercicios-das-aulas/RocketNotes/front-end/src/pages/SignIn/index.jsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles";
+
+export function SignIn () {
+    return (
+        <Container>
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+                <h2>Faça seu login</h2>
+            </Form>
+        </Container>
+    )
+}
