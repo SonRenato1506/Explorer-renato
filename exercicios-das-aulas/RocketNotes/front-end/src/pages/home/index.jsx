ee9d864816b2b24bf77247155ade88bbd541a169
Renato <renatimnerdin@gmail.com>
2023-06-03 10:47:11 -0300

    10:46

--- exercicios-das-aulas/RocketNotes/front-end/src/pages/home/index.jsx
+++ exercicios-das-aulas/RocketNotes/front-end/src/pages/home/index.jsx
@@ -1,17 +1,20 @@
-import {Container, Brand, Menu, Search, Content, NewNote} from "./styles"
+import {FiPlus} from "react-icons/fi"
+import { Container, Brand, Menu, Search, Content, NewNote } from "./styles"
 
 import { Header } from "../../components/Header"
 import { ButtonText } from "../../components/ButtonText"
 
-export function Home () {
+export function Home() {
     return (
         <Container>
             <Brand>
                 <h1>RocketNotes</h1>
             </Brand>
-            <Header/>
+            <Header />
             <Menu>
-
+                <li><ButtonText title="Todos" isActive/></li>
+                <li><ButtonText title="React"/></li>
+                <li><ButtonText title="Node"/></li>
             </Menu>
             <Search>
 
@@ -20,7 +23,8 @@ export function Home () {
 
             </Content>
             <NewNote>
-
+                <FiPlus />
+                Criar nota
             </NewNote>
         </Container>
     )
--- exercicios-das-aulas/RocketNotes/front-end/src/pages/home/styles.js
+++ exercicios-das-aulas/RocketNotes/front-end/src/pages/home/styles.js
@@ -22,27 +22,47 @@ export const Brand = styled.div`
     align-items: center;
     border-bottom-width: 1px;
     border-bottom-style: solid;
-    border-bottom-color: ${({theme}) => theme.COLORS.BACKGROUND_700};
-    background-color: ${({theme}) => theme.COLORS.BACKGROUND_900};
+    border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
 
     >h1 {
         font-size: 24px;
-        color: ${({theme}) => theme.COLORS.ORANGE};
+        color: ${({ theme }) => theme.COLORS.ORANGE};
     }
 `;
+
 export const Menu = styled.ul`
     grid-area: menu;
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+    padding-top: 64px;
+    text-align: center;
     
-`;
+    > li {
+        margin-bottom: 24px;
+    }
+    `;
+
 export const Search = styled.div`
     grid-area: search;
     
 `;
+
 export const Content = styled.div`
     grid-area: content;
     
 `;
+
 export const NewNote = styled.div`
     grid-area: newNote;
-    
+
+    background-color: ${({ theme }) => theme.COLORS.ORANGE}    ;
+    border: none;
+    display: flex;
+    align-items: center;
+    justify-content:center;
+
+    svg {
+        margin-right: 5px;
+    }
 `;
