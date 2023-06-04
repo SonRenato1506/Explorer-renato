6ec1566f00a59d24c8041df90d8400ecf291fa70
Renato <renatimnerdin@gmail.com>
2023-06-04 10:08:06 -0300

    Final do SigIn

--- exercicios-das-aulas/RocketNotes/front-end/src/pages/SignIn/index.jsx
+++ exercicios-das-aulas/RocketNotes/front-end/src/pages/SignIn/index.jsx
@@ -1,6 +1,10 @@
-import { Container } from "./styles";
+import { Container, Form, Background } from "./styles";
+import { FiMail, FiLock } from "react-icons/fi"
 
-export function SignIn () {
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
+
+export function SignIn() {
     return (
         <Container>
             <Form>
@@ -8,7 +12,26 @@ export function SignIn () {
                 <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
                 <h2>Faça seu login</h2>
+                <Input
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+                <Input
+                    placeholder="Senha"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Button title="Entrar"/>
+
+                <a href="#">
+                    Criar Conta
+                </a>
+                   
             </Form>
+            
+            <Background/>
         </Container>
     )
 }
