3cc1717fa6abe3e382c2859f734af3a6ce8dee03
Renato <renatimnerdin@gmail.com>
2023-06-04 10:54:55 -0300

    Cmc Profile

--- exercicios-das-aulas/RocketNotes/front-end/src/main.jsx
+++ exercicios-das-aulas/RocketNotes/front-end/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { SignUp } from './pages/SignUp/index.jsx'
+import { Profile } from './pages/profile/index.jsx'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme.js'
 import GlobalStyles from './styles/global.js'
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>,
 )
