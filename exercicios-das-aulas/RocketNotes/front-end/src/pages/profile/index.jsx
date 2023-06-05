31598854e0d9f602e22590f1cf8a19c798dd1c11
Renato <renatimnerdin@gmail.com>
2023-06-05 08:51:52 -0300

    Fazendo o Profile

--- exercicios-das-aulas/RocketNotes/front-end/src/pages/profile/index.jsx
+++ exercicios-das-aulas/RocketNotes/front-end/src/pages/profile/index.jsx
@@ -1,5 +1,7 @@
-import {FiArrowLeft} from "react-icons/fi"
-import { Container } from "./styles"
+import {FiArrowLeft, FiUser, FiMail, FiLock} from "react-icons/fi"
+import { Container, Form, Avatar } from "./styles"
+import { Input } from "../../components/Input"
+import {Button} from "../../components/Button"
 
 export function Profile () {
     return (
@@ -9,6 +11,32 @@ export function Profile () {
                     <FiArrowLeft/>
                 </a>
             </header>
+            <Form>
+                <Avatar>
+                    <img src="https://github.com/SonRenato1506.png" alt="Foto do Usuário" />
+                </Avatar>
+                <Input
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiUser}
+                />
+                <Input
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+                <Input
+                    placeholder="Senha atual"
+                    type="password"
+                    icon={FiLock}
+                />
+                <Input
+                    placeholder="Nova senha"
+                    type="password"
+                    icon={FiLock}
+                />
+                <Button title="Salvar"/>
+            </Form>
         </Container>
     )
 }
--- exercicios-das-aulas/RocketNotes/front-end/src/pages/profile/styles.js
+++ exercicios-das-aulas/RocketNotes/front-end/src/pages/profile/styles.js
@@ -4,6 +4,30 @@ export const Container = styled.div`
     width: 100%;
 
     > header {
-        width: 100%;
+        width: 83.65%;
+        height: 144px;
+
+        background-color: ${({theme}) => theme.COLORS.BACKGROUND_900};
+
+        display: flex;
+        align-items: center;
+
+        padding: 0 124px;
+       
+
+        svg {
+            color: ${({theme}) => theme.COLORS.GRAY_100};
+            font-size: 24px;
+
+        }
     }
+`
+
+export const Form = styled.form `
+    max-width: 340px;
+    margin: 30px auto 0;
+`
+
+export const Avatar = styled.div`
+
 `
