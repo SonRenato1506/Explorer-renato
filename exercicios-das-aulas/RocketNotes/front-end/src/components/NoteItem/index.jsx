24e241d9cece9ba0e687ca6c875967998c2fed12
Renato <renatimnerdin@gmail.com>
2023-06-05 20:53:31 -0300

    20:53

--- exercicios-das-aulas/RocketNotes/front-end/src/components/NoteItem/index.jsx
+++ exercicios-das-aulas/RocketNotes/front-end/src/components/NoteItem/index.jsx
@@ -6,7 +6,7 @@ export function NoteItem ({isNew, vslue, onClick, ...rest}) {
     return (
         <Container isNew={isNew}>
             <input type="text" value={value} readOnly={!isNew} {...rest}/>
-            <button type="button" onClick={onClick} {isNew ? <FiPlus/> : <Fix/>}></button>
+            <button type="button" onClick={onClick} >{isNew ? <FiPlus/> : <Fix/>}</button>
 
         </Container>
     )
